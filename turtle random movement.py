e91df2d3cb9a7775f1212fcc513ef26438d8508e
3zom5u <jude.dommett@gmail.com>
2019-04-27 11:40:17 +0100

    Add files via upload

--- turtle random movement.py
+++ turtle random movement.py
@@ -5,16 +5,29 @@ import random
 from random import randint
 r = lambda: random.randint(0,255)
 hexcolour = '#%02X%02X%02X' % (r(),r(),r())
+shapesize(5)
 speed(0)
-width(50)
+width(5)
+hideturtle()
 poscheckerx = 0
 poscheckery = 0
+penup()
+goto(320,290)
+pendown()
+goto(-330,290)
+goto(-330,-280)
+goto(320,-280)
+goto(320,290)
+penup()
 sety(0)
 setx(0)
 while True:
     pencolor(hexcolour)
-    right(randint(0, 180))
+    right(randint(0, 360))
     forward(20)
+    x = stamp()
+    y = x - 50
+    clearstamp(y)
     poscheckerx = round(xcor(), 5)
     poscheckery = round(ycor(), 5)
     if poscheckerx >= 311.50 or poscheckerx <= -321.50:
@@ -24,3 +37,4 @@ while True:
         right(180)
         forward(30)
     hexcolour = '#%02X%02X%02X' % (r(),r(),r())
+    
