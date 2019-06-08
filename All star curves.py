ea5de2654fe1dc05c9ea69b696a589dab83b4a2c
3zom5u <jude.dommett@gmail.com>
2019-06-08 11:09:35 +0100

    Update All star curves.py

--- All star curves.py
+++ All star curves.py
@@ -2,51 +2,49 @@ from turtle import *
 import time
 import random
 from random import randint
-print("how many points do you want your star to have?(it can be anything!!!)")
-points = int(input())
-print("how big do you want it?(100-350 but gets slower the bigger the size")
-distance = int(input())
-turn = 360/points
-listnumber = 0
-listnumber2 = distance
-line1x = []
-line1y = []
-line2x = []
-line2y = []
-penup()
-speed(0)
-left(90)
-#hideturtle()
-for i in range(points):#to make each part of the star
-    for i in range(distance + 1):#doing line1 positions
-        forward(1)
-        line1x.append(xcor())
-        line1y.append(ycor())
-    goto(0,0)
-    right(turn)
-    for i in range(distance + 1):#doing line2 positions
-        forward(1)
-        line2x.append(xcor())
-        line2y.append(ycor())
-    goto(0,0)
-    pendown()
-    for i in range(distance + 1):#drawing
-        goto(line1x[listnumber],line1y[listnumber])
-        goto(line2x[listnumber2],line2y[listnumber2])
-        listnumber = listnumber + 1
-        listnumber2 = listnumber2 - 1
-    goto(0,0)
-    line1x.clear()
-    line1y.clear()
-    line2x.clear()
-    line2y.clear()
+import mlib
+from mlib import points
+def thething():
+    points = int(input("how many points would you like your star to have?(it can be anything!)"))
+    distance = int(input("how big would you like your star to be?(200-350 recommended)"))
+    turn = 360/points
+    inc = distance/100
     listnumber = 0
-    listnumber2 = distance
-
-
-
-
-
+    listnumber2 = 99
+    line1x = []
+    line1y = []
+    line2x = []
+    line2y = []
+    speed(0)
+    left(90)
+    hideturtle()
+    for i in range(points):#to make each part of the star
+        penup()
+        for i in range(100):#doing line1 positions
+            forward(inc)
+            line1x.append(xcor())
+            line1y.append(ycor())
+        goto(0,0)
+        right(turn)
+        for i in range(100):#doing line2 positions
+            forward(inc)
+            line2x.append(xcor())
+            line2y.append(ycor())
+        goto(0,0)
+        pendown()
+        for i in range(100):#drawing
+            goto(line1x[listnumber],line1y[listnumber])
+            goto(line2x[listnumber2],line2y[listnumber2])
+            listnumber = listnumber + 1
+            listnumber2 = listnumber2 - 1
+            goto(0,0)
+        line1x.clear()
+        line1y.clear()
+        line2x.clear()
+        line2y.clear()
+        listnumber = 0
+        listnumber2 = 99
+thething()
 
 
 
